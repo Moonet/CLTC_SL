0700a05c56b3e6a778699b0039cb29591bd545a8
Moonet <xdongxdu@gmail.com>
2019-10-30 22:15:11 -0400

    regular update

--- run_ld.py
+++ run_ld.py
@@ -132,14 +132,7 @@ class MLDProcessor(DataProcessor):
     def __init__(self, lang, tokenizer):
         self.lang = lang
         self.tokenizer = tokenizer
-        with open('en-ja.txt', "r", encoding='utf-8') as f:
-            reader = csv.reader(f, delimiter="\t", quotechar=None)
-            dict_en2zh = {}
-            for line in reader:
-                en = line[0]
-                zh = line[1]
-                dict_en2zh[en] = zh
-        self.en2zh = dict_en2zh
+
 
     def get_train_examples(self, data_dir):
         """See base class."""
@@ -151,32 +144,7 @@ class MLDProcessor(DataProcessor):
             guid = "%s-%s" % ('train', i)
             text_a = line[1]
             label = line[0]
-            text_a_list = text_a.split()
-            new_text_a = []
-            for word in text_a_list:
-                if word in self.en2zh.keys():
-                    new_text_a.append(self.en2zh[word])
-                else:
-                    new_text_a.append(word)
-            # text_a_wr = ' '.join(new_text_a)
-            examples.append(
-                InputExample(guid=guid, text_a=text_a, label=label))
-            # examples.append(
-            #     InputExample(guid=guid, text_a=text_a_wr, label=label))
-
-        lines = self._read_tsv(os.path.join(data_dir, self.lang + ".train.1000"))
-        for (i, line) in enumerate(lines):
-            guid = "%s-%s" % ('train', i)
-            text_a = line[1]
-            label = line[0]
-            text_a_list = text_a.split()
-            new_text_a = []
-            for word in text_a_list:
-                if word in self.en2zh.keys():
-                    new_text_a.append(self.en2zh[word])
-                else:
-                    new_text_a.append(word)
-            # text_a_wr = ' '.join(new_text_a)
+            
             examples.append(
                 InputExample(guid=guid, text_a=text_a, label=label))
 
@@ -412,42 +380,6 @@ def warmup_linear(x, warmup=0.002):
         return x / warmup
     return 1.0 - x
 
-
-def imq_kernel(X: torch.Tensor,
-               Y: torch.Tensor,
-               h_dim: int):
-    batch_size = X.size(0)
-
-    p2_norm_x = X.pow(2).sum(1).unsqueeze(0)
-    norms_x = X.sum(1).unsqueeze(0)
-    prods_x = torch.mm(norms_x, norms_x.t())
-    dists_x = p2_norm_x + p2_norm_x.t() - 2 * prods_x
-
-    p2_norm_y = Y.pow(2).sum(1).unsqueeze(0)
-    norms_y = X.sum(1).unsqueeze(0)
-    prods_y = torch.mm(norms_y, norms_y.t())
-    dists_y = p2_norm_y + p2_norm_y.t() - 2 * prods_y
-
-    dot_prd = torch.mm(norms_x, norms_y.t())
-    dists_c = p2_norm_x + p2_norm_y.t() - 2 * dot_prd
-
-    stats = 0
-    for scale in [.1, .2, .5, 1., 2., 5., 10.]:
-        C = 2 * h_dim * 1.0 * scale
-        res1 = C / (C + dists_x)
-        res1 += C / (C + dists_y)
-
-        if torch.cuda.is_available():
-            res1 = (1 - torch.eye(batch_size).cuda()) * res1
-        else:
-            res1 = (1 - torch.eye(batch_size)) * res1
-
-        res1 = res1.sum() / (batch_size - 1)
-        res2 = C / (C + dists_c)
-        res2 = res2.sum() * 2. / (batch_size)
-        stats += res1 - res2
-
-    return stats
 
 
 def sort(train_examples, eval_examples, label_list, id2conf, num_k):
